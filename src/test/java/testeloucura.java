b20dc14af2a4361b72cd3ec015a5bd8eaaa70531
mrl00 <m2l_0@outlook.com>
2016-07-28 15:47:12 -0300

    Fudeo

--- src/test/java/testeloucura.java
+++ src/test/java/testeloucura.java
@@ -1,9 +1,8 @@
 
-import bancodedados.Queries;
-import bancodedados.SimpleQueries;
-import java.util.ArrayList;
-import java.util.List;
+import entity.EntityModifiable;
 import entity.Gesac;
+
+
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -15,15 +14,7 @@ import entity.Gesac;
  * @author murilo
  */
 public class testeloucura {
-
     public static void main(String[] args) {
-      
-        List<Gesac> gt = new ArrayList<>();
-        Queries q = new SimpleQueries();
-  
-        for (int i = 0; i < 10; i++) {
-            gt.add(new Gesac(i, "teste".concat(String.valueOf(i))));
-            q.insert(gt.get(i));
-        }
+        EntityModifiable em = new Gesac(33, "ssssss");
     }
 }
