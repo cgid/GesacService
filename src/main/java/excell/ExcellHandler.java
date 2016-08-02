3c3a6b5c68851bc5518dddf17fa1ad51c557c3cf
edillsonjr <edilsonjuniormvf@gmail.com>
2016-08-02 13:49:36 -0300

    adicionando jar

--- src/main/java/excell/ExcellHandler.java
+++ src/main/java/excell/ExcellHandler.java
@@ -5,10 +5,41 @@
  */
 package excell;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
 /**
  *
  * @author Edilson Jr
  */
 public class ExcellHandler {
-    
+
+    public void readSheet() {
+        Workbook workbook;
+        try {
+            workbook = Workbook.getWorkbook(new File("Itapuranga.xls"));
+
+            Sheet sheet = workbook.getSheet(0);
+
+            int numColunas = sheet.getColumns();
+            int numero[] = null;
+            String nomedascolunas[] = null;
+            System.out.println("Iniciando a leitura da planilha XLS:");
+            for (int i = 1; i < numColunas; i++) {
+                Cell a6 = sheet.getCell(i, 0);
+
+                numero[i] = a6.getColumn();
+                nomedascolunas[i] = a6.getContents();
+
+            }
+        } catch (IOException | BiffException ex) {
+            Logger.getLogger(ExcellHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
