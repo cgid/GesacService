cd13d9f4a245b2254e41a456a6a3bb88751d81e7
mrl00 <m2l_0@outlook.com>
2016-08-03 10:29:15 -0300

    Atualizacao ExcellHandler.

--- src/main/java/excell/ExcellHandler.java
+++ src/main/java/excell/ExcellHandler.java
@@ -22,21 +22,39 @@ public class ExcellHandler {
 
     public void readSheet() {
         Workbook workbook;
+        Sheet sheet;
+        Cell a6;
         try {
             workbook = Workbook.getWorkbook(new File("Itapuranga.xls"));
-
-            Sheet sheet = workbook.getSheet(0);
-
-            int numColunas = sheet.getColumns();
-            int numero[] = null;
-            String nomedascolunas[] = null;
+            sheet = workbook.getSheet(0);            
+            
             System.out.println("Iniciando a leitura da planilha XLS:");
-            for (int i = 1; i < numColunas; i++) {
-                Cell a6 = sheet.getCell(i, 0);
-
-                numero[i] = a6.getColumn();
-                nomedascolunas[i] = a6.getContents();
-
+            for (int i = 2; i <= sheet.getRows(); i++) {
+                for (int j = 1; j <= sheet.getColumns(); j++) {
+                    a6 = sheet.getCell(i, j);
+                    switch(j) {
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        case 5:
+                            break;
+                        case 6:
+                            break;
+                        case 7:
+                            break;
+                        case 8:
+                            break;    
+                        case 9:
+                            break;    
+                        case 10:
+                            break;   
+                    }
+                }
             }
         } catch (IOException | BiffException ex) {
             Logger.getLogger(ExcellHandler.class.getName()).log(Level.SEVERE, null, ex);
